d2ed76f3ba228a42c25d2a9684c45b1e599a89a9
ryo-fukumoto <gkrpw6767@gmail.com>
2019-09-24 11:36:52 +0900

    top-page-view
    トップページのビュー作成

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
   def index
+    @categories = Category.all
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Category.create(name:"HTML")
+Category.create(name:"CSS")
+Category.create(name:"Ruby")
+Category.create(name:"Ruby on Rails")
+Category.create(name:"Javascript")
+Category.create(name:"jQuery")
+Category.create(name:"PHP")
