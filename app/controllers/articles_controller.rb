daa9545c508777ba1de29801a9fce84a79bc6356
ryo-fukumoto <gkrpw6767@gmail.com>
2019-09-26 19:03:36 +0900

    記事新規投稿画面
    記事の新規投稿ページの作成

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,9 @@ class ArticlesController < ApplicationController
   def index
     @categories = Category.all
   end
+
+  def new
+    @categories = Category.all
+    @article = Article.new
+  end
 end
