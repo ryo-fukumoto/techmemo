0ea048c246215443db358f9284328d2fa9a8c67b
ryo-fukumoto <gkrpw6767@gmail.com>
2019-09-28 22:08:27 +0900

    gem_kaminari
    トップページ記事一覧のページネーションの導入

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
   def index
     @categories = Category.all
+    @articles = Article.includes(:user).page(params[:page]).per(5).order("created_at DESC")
   end
 
   def new
