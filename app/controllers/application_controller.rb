97935eda1a723ec159ace4b88575d50682ecc89d
ryo-fukumoto <gkrpw6767@gmail.com>
2019-10-13 20:20:44 +0900

    application_controller_beforeaction
    カテゴリ一覧を呼び出すためのアクションをapplication_controllerに設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
     before_action :configure_permitted_parameters, if: :devise_controller?
+    before_action :categories_menu
 
     def after_sign_out_path_for(resource)
       '/users/sign_in' # サインアウト後のリダイレクト先URL
@@ -9,5 +10,9 @@ class ApplicationController < ActionController::Base
 
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    end
+
+    def categories_menu
+      @categories = Category.all
     end
 end
