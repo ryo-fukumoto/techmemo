4f4b995d9ac608c386384b8bf5e07d33903af81e
ryo-fukumoto <gkrpw6767@gmail.com>
2019-10-16 22:26:34 +0900

    users_show_view
    特定ユーザーの投稿一覧を表示するビュー作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
-    user = User.find(params[:id])
-    @article = user.article.page(params[:page]).per(20).order("created_at DESC")
+    @user = User.find(params[:id])
+    @articles = @user.articles.page(params[:page]).per(20).order("created_at DESC")
   end
 end
