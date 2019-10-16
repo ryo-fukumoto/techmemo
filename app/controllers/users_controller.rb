21675c3c530a9e2cf6750bbf69ae3f03d12edce2
ryo-fukumoto <gkrpw6767@gmail.com>
2019-10-16 22:18:26 +0900

    users_show_action
    users_controllerのshowアクションの定義

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,6 @@
 class UsersController < ApplicationController
+  def show
+    user = User.find(params[:id])
+    @article = user.article.page(params[:page]).per(20).order("created_at DESC")
+  end
 end
